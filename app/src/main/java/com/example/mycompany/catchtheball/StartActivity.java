3ccb9e5724c9c2c7f3871d0bd32c9f73f904c980
ikuto_murakami <m.ikuto@icloud.com>
2020-11-26 03:25:27 +0900

    MainActivity へ画面遷移

--- app/src/main/java/com/example/mycompany/catchtheball/StartActivity.java
+++ app/src/main/java/com/example/mycompany/catchtheball/StartActivity.java
@@ -2,7 +2,9 @@ package com.example.mycompany.catchtheball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void startGame(View view){
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
