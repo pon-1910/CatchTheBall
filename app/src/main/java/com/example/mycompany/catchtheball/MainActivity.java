774548647e7e032fdac62fc4f205f80bf10abd9a
ikuto_murakami <m.ikuto@icloud.com>
2020-11-26 02:50:15 +0900

    ResultActivity に画面遷移

--- app/src/main/java/com/example/mycompany/catchtheball/MainActivity.java
+++ app/src/main/java/com/example/mycompany/catchtheball/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mycompany.catchtheball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
@@ -167,6 +168,10 @@ public class MainActivity extends AppCompatActivity {
                 timer = null;
             }
 
+            // 結果画面へ
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            intent.putExtra("SCORE", score);
+            startActivity(intent);
         }
     }
 
